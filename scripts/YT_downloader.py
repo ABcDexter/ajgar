3d2c95ab215c4b6c04c7079312ce89c26ca9ee47
ABcDexter <abc.de.gen.x@gmail.com>
2026-06-21 22:09:56 +0530

    minor changes to the YT downloader script

--- scripts/YT_downloader.py
+++ scripts/YT_downloader.py
@@ -1,10 +1,10 @@
 import yt_dlp
 
-def download_youtube_as_mp3(video_url):
+def download_youtube_as_mp3(video_url: str, file_name: str) -> None:
     # Configure downloading options
     ydl_opts = {
         'format': 'bestaudio/best',      # Fetch the highest quality audio stream
-        'outtmpl': '%(title)s.%(ext)s',  # Set output filename to video title
+        'outtmpl': f'{file_name}.mp3',   # Set output filename to user-specified name
         'postprocessors': [{             # Extract and convert audio to MP3 using FFmpeg
             'key': 'FFmpegExtractAudio', # Use FFmpeg to extract audio
             'preferredcodec': 'mp3',     # mp3 format
@@ -23,4 +23,6 @@ def download_youtube_as_mp3(video_url):
 if __name__ == "__main__":
     # Prompt user for the URL
     url = input("Please enter the YouTube Video URL: ")
-    download_youtube_as_mp3(url)
+    output_file_name = input("Please enter the desired output filename (without extension): ")
+    # Set the output file_name to the user-specified filename
+    download_youtube_as_mp3(url, output_file_name)
