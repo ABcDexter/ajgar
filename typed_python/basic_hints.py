5bea1f7b20a3de4fc6e407773fa028bfbeb752bd
ABcDexter <abc.de.gen.x@gmail.com>
2026-07-07 22:45:52 +0530

    Began basic type_hints

---
+++ typed_python/basic_hints.py
@@ -0,0 +1,11 @@
+############################
+# Basic Type Hints in Python
+############################
+def add_two_numbers(a: int, b:int) -> int:
+    '''
+    Adds two numbers and return the sum
+    '''
+    return a+b
+
+print(1+2) # simply sums
+print('Anubhav' + 'Balodhi')  # Does nothing to enfore types  but concatenates
