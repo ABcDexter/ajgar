e8001ac18740af23849c920249ecaae5e84068a9
ABcDexter <abc.de.gen.x@gmail.com>
2026-07-10 22:13:29 +0530

    Added all Widgets info

--- pyqt6/widgets/lesson_001_basic_QtWidgets.py
+++ pyqt6/widgets/lesson_001_basic_QtWidgets.py
@@ -24,24 +24,33 @@ from PyQt6.QtWidgets import (
     QWidget,
 )
 
+src = "https://www.pythonguis.com/tutorials/pyqt6-widgets/"
 
-doc_string = """Widgets App (src = https://www.pythonguis.com/tutorials/pyqt6-widgets/)
+doc_string = f"""Widgets App ({src=})
 In Qt (and most User Interfaces), widget is the name given to a component of the UI that the user can interact with. User interfaces are made up of multiple widgets, arranged within the window.
 
 This example shows a simple window with a variety of widgets.
 The window contains a vertical layout with the following widgets:
-- QCheckBox
-- QComboBox
-- QDateEdit
-- QDateTimeEdit
-- QDial
-- QDoubleSpinBox
-- QFontComboBox
-- QLCDNumber
-- QLabel
-- QLineEdit
-- QProgressBar
-- QPushButton
+{'_' * 80}
+Widget	       |  What it does
+{'_' * 80}
+QCheckBox	   |  A checkbox
+QComboBox	   |  A dropdown list box
+QDateEdit	   |  For editing dates
+QDateTimeEdit  |  For editing dates and datetimes
+QDial          |  Rotatable dial
+QDoubleSpinBox |  A number spinner for floats
+QFontComboBox  |  A list of fonts
+QLCDNumber	   |  A quite ugly LCD display
+QLabel		   |  Just a label, not interactive
+QLineEdit	   |  Enter a line of text
+QProgressBar   |  A progress bar
+QPushButton	   |  A button
+QRadioButton   |  A toggle set, with only one active item
+QSlider        |  A slider
+QSpinBox       |  An integer spinner
+QTimeEdit      |  For editing times
+
 """
 
 print(f"{doc_string}{'_' * 80}")
