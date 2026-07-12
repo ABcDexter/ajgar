5f24d984d083bd9e2b663a01e3556ff10c9b49da
ABcDexter <abc.de.gen.x@gmail.com>
2026-07-12 22:06:35 +0530

    Added doc_str

--- pyqt6/widgets/lesson_004_QComboBox.py
+++ pyqt6/widgets/lesson_004_QComboBox.py
@@ -1,7 +1,24 @@
 import sys
+import time
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QApplication, QComboBox, QMainWindow
 
+
+doc_str = f"""
+The QComboBox is a drop-down list, closed by default with an arrow to open it.
+We can also set a flag to determine how the insert is handled.
+{"_"*79}
+PyQt6 flag (long name)                      | Behavior
+{"_"*79}
+QComboBox.InsertPolicy.NoInsert             | No insert
+QComboBox.InsertPolicy.InsertAtTop          | Insert as first item
+QComboBox.InsertPolicy.InsertAtCurrent      | Replace currently selected item
+QComboBox.InsertPolicy.InsertAtBottom       | Insert after last item
+QComboBox.InsertPolicy.InsertAfterCurrent   | Insert after current item
+QComboBox.InsertPolicy.InsertBeforeCurrent  | Insert before current item
+QComboBox.InsertPolicy.InsertAlphabetically | Insert in alphabetical order
+{"_"*79}
+"""
 
 class MainWindow(QMainWindow):
 
@@ -27,7 +44,8 @@ class MainWindow(QMainWindow):
     def text_changed(self, s:str):
         print(f"Text changed to {s}")
 
-
+print(doc_str)
+time.sleep(1)
 app = QApplication(sys.argv)
 w = MainWindow()
 w.show()
